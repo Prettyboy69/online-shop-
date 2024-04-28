a470071e28f3444a9d206670f35ef7d5a737a7b9
Prettyboy69 <popoolamoses40@gsmil.com>
2024-04-28 04:15:05 -0700

    commited

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg bg-body-tertiarywhite py-3 shadow-sm">
   <div className="container">
     <NavLink className="navbar-brand fw-bold fs-4" to="#">
-        LA COLLECTION 
+        LA COLLECTIONS 
         </NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
